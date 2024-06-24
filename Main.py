68333027fc5d7e1e9aa079aed4d9423cd29b50f9
lukevukovich <lukevuke83@gmail.com>
2024-06-23 20:54:39 -0400

    Improve algorithm to find shortest path

--- Main.py
+++ Main.py
@@ -2,8 +2,10 @@ import pygame
 from src.BoardLoader import BoardLoader
 from src.PathFinder import PathFinder
 import sys
+from src.PathFinder import Direction
+import itertools
 
-TILE_SIZE = 40
+TILE_SIZE = 30
 
 
 def pygame_init(width: int, height: int):
@@ -23,27 +25,38 @@ def main():
     """
     Main function and event loop.
     """
+    # Load the board and start position
     board_file = sys.argv[1]
     board_loader = BoardLoader(board_file)
     tile_board = board_loader.get_tile_board()
     start_pos = board_loader.get_start()
 
-    path_finder = PathFinder(tile_board, start_pos)
-    maze_path = path_finder.get_path()
+    # Find shortest path by permuting the directions
+    directions = [Direction.LEFT, Direction.RIGHT, Direction.UP, Direction.DOWN]
+    permutations = list(itertools.permutations(directions))
+    maze_paths = []
+    for perm in permutations:
+        path_finder = PathFinder(tile_board, start_pos, perm)
+        maze_path = path_finder.get_path()
+        maze_paths.append(maze_path)
+    shortest_path = min(maze_paths, key=len)
 
+    # Initialize the pygame screen
     height = len(tile_board) * TILE_SIZE
     width = len(tile_board[0]) * TILE_SIZE
     screen = pygame_init(width, height)
 
+    # Event loop
     solved = False
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+            # Toggle the shortest path
             elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
                 if not solved:
-                    board_loader.set_maze_path(maze_path)
+                    board_loader.set_maze_path(shortest_path)
                     solved = True
                 else:
                     board_loader.remove_maze_path()
@@ -51,6 +64,7 @@ def main():
 
         screen.fill((0, 0, 0))
 
+        # Draw the tiles
         for row in tile_board:
             for tile in row:
                 color = tile.get_color()
--- src/PathFinder.py
+++ src/PathFinder.py
@@ -36,9 +36,10 @@ class PathFinder:
     """
     A class to find the path from the start to the end of a maze.
     """
-    def __init__(self, tile_board: list[list[MazeTile]], start_pos: tuple[int, int]):
+    def __init__(self, tile_board: list[list[MazeTile]], start_pos: tuple[int, int], directions: list[Direction] = None):
         self.tile_board = copy.deepcopy(tile_board)
         self.start_pos = start_pos
+        self.directions = directions
         self.path_board = self.init_path_board()
         self.start_path = self.path_board[self.start_pos[0]][self.start_pos[1]]
         self.path = self.find_path()
@@ -58,7 +59,7 @@ class PathFinder:
     def find_path(self) -> list[Path]:
         """
         Finds the path from the start to the end of the maze.
-        Returns a list of Path objects.
+        Returns a list of Path objects if a path exists, otherwise returns an empty list.
         """
         top = self.start_path
         top_tile = top.get_tile()
@@ -67,9 +68,18 @@ class PathFinder:
         dead_end = True
         stack = [top]
         coord_stack = [(top_tile.get_x(), top_tile.get_y())]
-        while True:
+        visited = set(coord_stack)
+        
+        while stack:
+            top = stack[-1]
+            top_tile = top.get_tile()
 
-            for direction in Direction:
+            if top_tile.get_tile_type() == MazeTileType.END:
+                return stack
+
+            # Check all directions for a valid path
+            dead_end = True
+            for direction in self.directions if self.directions else Direction:
                 x, y = top_tile.get_x(), top_tile.get_y()
                 y += direction.value[0]
                 x += direction.value[1]
@@ -79,37 +89,38 @@ class PathFinder:
                 except IndexError:
                     continue
 
+                # Check that node is valid
                 if (
                     new_top.get_tile().get_tile_type() != MazeTileType.WALL
-                    and (x, y) not in coord_stack
+                    and (x, y) not in visited
                 ):
+                    # Add to stack, mark as visited, and set backtrack to True
                     new_top.set_backtrack(backtrack)
                     stack.append(new_top)
                     coord_stack.append((x, y))
+                    visited.add((x, y))
 
                     backtrack = True
                     dead_end = False
+                    break
 
             if dead_end:
-                while True:
-                    if top_tile.get_tile_type == MazeTileType.START:
-                        return
-                    elif not top.is_backtrack():
-                        stack.pop()
-                        top = stack[-1]
-                        top_tile = top.get_tile()
-                    else:
-                        stack.pop()
+                while stack:
+                    top = stack[-1]
+                    top_tile = top.get_tile()
+                    if top_tile.get_tile_type() == MazeTileType.START:
+                        return []  # No path found
+                    stack.pop()
+                    coord_stack.pop()
+                    if not stack:
+                        return []  # No path found
+                    if stack and not stack[-1].is_backtrack():
                         break
 
-            top = stack[-1]
-            top_tile = top.get_tile()
-
             backtrack = False
-            dead_end = True
 
-            if top_tile.get_tile_type() == MazeTileType.END:
-                return stack
+        return []  # If the stack is empty and no path was found, return an empty list
+
             
     def clean_path(self) -> None:
         """
